ce6dc81dae523962bf7b16a910a7ef217dc324aa
twilly27 <jordan.kelly1@mds.ac.nz>
2022-10-25 17:54:07 +1300

    Undid the test comment

--- Project/DetectionBlur.py
+++ Project/DetectionBlur.py
@@ -6,4 +6,4 @@ original[0:500, 0:500] = blurred[0:500, 0:500]
 
 #1358.99560546875, 293.4925231933594, 152.9748992919922, 261.75750732421875, 23.010772705078125
 
-cv2.imwrite('cvBlurredOutput.jpg', original) #test
+cv2.imwrite('cvBlurredOutput.jpg', original)
