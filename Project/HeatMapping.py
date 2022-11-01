3851f4129bd4cad16b9fadea02c597573afe61a7
Hui Yung Yoon (Keith) <49555123+iDeepDev8@users.noreply.github.com>
2022-11-01 15:31:07 +1300

    Adding Streamlit.py and HeatMapping.py back

--- Project/HeatMapping.py
+++ Project/HeatMapping.py
@@ -1,17 +1,47 @@
+from tkinter import font
+import pandas as pd
+from PIL import Image
+import matplotlib.image as img
+
 from scipy.interpolate import Rbf  # radial basis functions
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
-x = [1555,1203,568,1098,397,564,1445,337,1658,1517,948]
-y = [860,206,1097,425,594,614,553,917,693,469,306]
-x = [0.9, 0.6, 0.1, 0.5, 0.04, 0.1, 0.82, 0.0, 1.0, 0.89, 0.46]
-y = [0.73, 0.0, 1.0, 0.24, 0.43, 0.45, 0.38, 0.7, 0.54, 0.29, 0.11]
-z = [1]*len(x)
+datafile = 'Dataset.json'
+with open(datafile) as f:
+    data = json.load(f)
+
+df = pd.DataFrame()
+for n in range(len(data)):
+    id =[]
+    bbox = []
+    score = []
+    for i, key in enumerate(data[n].keys()):
+        print()
+        if i == 0:
+            id.append(data[n][key])
+        if i == 1:
+            bbox.append(data[n][key])
+        if i == 2:
+            score.append(data[n][key])
+    temp = pd.DataFrame({'id': id, 'x_box': bbox[0][0], 'y_box': bbox[0][1], 'w_box': bbox[0][2], 'h_box': bbox[0][3], 'a_box': bbox[0][4], 'score': score})
+    #     # concat dataframes
+    df = pd.concat([df, temp], ignore_index=True)
+
+# get the first column of df
+x = df[df.columns[1]]
+y = df[df.columns[2]]
+# normalize the data attributes
+x = (x-x.min())/ (x.max() - x.min())
+y = (y-y.min())/ (y.max() - y.min())
+
+z = [1]*len(x) 
 
 rbf_adj = Rbf(x, y, z, function='gaussian')
 
-x_fine = np.linspace(0, 1, 81)
-y_fine = np.linspace(0, 1, 82)
+x_fine = np.linspace(-1, 2, 82)
+y_fine = np.linspace(-1, 2, 82)
 
 x_grid, y_grid = np.meshgrid(x_fine, y_fine)
 
@@ -19,8 +49,8 @@ z_grid = rbf_adj(x_grid.ravel(), y_grid.ravel()).reshape(x_grid.shape)
 
 plt.gca().invert_yaxis()
 #plt.gca().invert_xaxis()
-plt.pcolor(x_fine, y_fine, z_grid);
-plt.plot(x, y, 'ok');
-plt.xlabel('x'); plt.ylabel('y'); plt.colorbar();
-plt.title('Heat Intensity Map');
+plt.pcolor(x_fine, y_fine, z_grid)
+plt.plot(x, y, 'ok')
+plt.xlabel('x'), plt.ylabel('y'), plt.colorbar()
+plt.title('Heat Intensity Map')
 plt.show()
